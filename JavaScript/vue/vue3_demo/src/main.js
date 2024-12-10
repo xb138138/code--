36661fdaca9804576c74e020c59f4e9e4db12d6f
xb <1439545090@qq.com>
2024-12-10 10:44:22 +0800

    JavaScript/vue/vue3_demo/src/05App.vue

--- JavaScript/vue/vue3_demo/src/main.js
+++ JavaScript/vue/vue3_demo/src/main.js
@@ -3,7 +3,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from './05App.vue'
 import router from './router'
 
 const app = createApp(App)
